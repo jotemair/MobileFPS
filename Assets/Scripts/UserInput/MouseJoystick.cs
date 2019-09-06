c53b108b7fb2afc19e442f2c5477bf5bf7ab6f44
Adam Rudas <jotemair@gmail.com>
2019-09-06 14:57:56 +1000

    Make sure mouse joystick script only runs on PC

--- Assets/Scripts/UserInput/MouseJoystick.cs
+++ Assets/Scripts/UserInput/MouseJoystick.cs
@@ -28,12 +28,20 @@ public class MouseJoystick : MonoBehaviour
 
     private bool _lockCursor = false;
 
+    private bool _isOnPC = true;
+
     #endregion
 
     #region Functions
 
     private void Start()
     {
+        if ((Application.platform != RuntimePlatform.Android) && (Application.platform != RuntimePlatform.IPhonePlayer))
+        {
+            Destroy(this);
+            _isOnPC = false;
+        }
+
         _joystick = GetComponent<Joystick>();
         Assert.IsNotNull(_joystick, "KeyboardJoystick component should be placed on a GameObject with a Joystick component");
     }
@@ -41,31 +49,34 @@ public class MouseJoystick : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _lockCursor = ((GameManager.Instance.GameState == GameManager.GameStates.Game) || (GameManager.Instance.GameState == GameManager.GameStates.Wipe)) ;
-        Cursor.lockState = (_lockCursor ? CursorLockMode.Locked : CursorLockMode.None);
-        Cursor.visible = !_lockCursor;
-
-        if (_lockCursor)
+        if (_isOnPC)
         {
-            // Get mouse input vector
-            Vector2 mouseVector = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+            _lockCursor = ((GameManager.Instance.GameState == GameManager.GameStates.Game) || (GameManager.Instance.GameState == GameManager.GameStates.Wipe));
+            Cursor.lockState = (_lockCursor ? CursorLockMode.Locked : CursorLockMode.None);
+            Cursor.visible = !_lockCursor;
 
-            // When generated keyboard vector is over set distance away from previous value, lerp will be applied to smooth out input
-            if (Vector2.Distance(_lastInputVector, mouseVector) > _lerpTreshold)
+            if (_lockCursor)
             {
-                mouseVector = Vector2.Lerp(_lastInputVector, mouseVector, Time.deltaTime * _lerpSpeed);
-            }
-            _lastInputVector = mouseVector;
+                // Get mouse input vector
+                Vector2 mouseVector = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
 
-            // Check if we have a zero input vector
-            bool hasZeroMagnitude = (Vector2.zero == mouseVector);
+                // When generated keyboard vector is over set distance away from previous value, lerp will be applied to smooth out input
+                if (Vector2.Distance(_lastInputVector, mouseVector) > _lerpTreshold)
+                {
+                    mouseVector = Vector2.Lerp(_lastInputVector, mouseVector, Time.deltaTime * _lerpSpeed);
+                }
+                _lastInputVector = mouseVector;
 
-            // Only send input if it's not zero, or if we did not send zero yet
-            // This allows mouse controls when keyboard is not used
-            if (!hasZeroMagnitude || !_sentZero)
-            {
-                _joystick.HandleInput(mouseVector);
-                _sentZero = hasZeroMagnitude;
+                // Check if we have a zero input vector
+                bool hasZeroMagnitude = (Vector2.zero == mouseVector);
+
+                // Only send input if it's not zero, or if we did not send zero yet
+                // This allows mouse controls when keyboard is not used
+                if (!hasZeroMagnitude || !_sentZero)
+                {
+                    _joystick.HandleInput(mouseVector);
+                    _sentZero = hasZeroMagnitude;
+                }
             }
         }
     }
